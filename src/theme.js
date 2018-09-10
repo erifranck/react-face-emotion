7fe2479dc1084eaada57fa1d3bda6f2fefa34ff9
erifranck <erifranjose@hotmail.com>
2018-09-10 06:20:31 -0500

    create theme

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+const colorpalette = {
+    blue: "#448EF6",
+    white: "#FFFFFF",
+    yellow: "#FFE981",
+    aqua: "#65DAF7",
+    grey: "#75C2F6"
+}
+
+export const theme = {
+  palette: {
+    ...colorpalette
+  },
+  colors: {
+    primary: colorpalette.blue,
+    secondary: colorpalette.yellow,
+    inverse: colorpalette.white
+  }
+}
