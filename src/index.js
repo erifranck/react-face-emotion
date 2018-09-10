760be612bfc96a9e6bab32715951954be686ea44
erifranck <erifranjose@hotmail.com>
2018-09-10 06:19:10 -0500

    import global-styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from 'containers'
+import {ThemeProvider} from 'styled-components'
+import {theme} from 'theme'
+import {Provider} from 'react-redux'
+import store from 'redux/createStore'
+import 'global-styles'
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(
+  (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  )
+  ,
+  document.getElementById("root"))
