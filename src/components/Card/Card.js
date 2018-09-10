90055f171f5d13e11e6e1490addeacfc6d9df074
erifranck <erifranjose@hotmail.com>
2018-09-10 06:20:06 -0500

    set flip animation

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,4 +2,39 @@ import styled from 'styled-components'
 import CardComponent from './Card.jsx'
 
 export const Card = styled(CardComponent)`
+  width: 420px;
+  height: 280px;
+  perspective: 1000px;
+  .card-content{
+    transition: 0.6s;
+    transform-style: preserve-3d;
+    position: relative;
+    border: 1px solid #eee;
+  }
+  .card-content.flip {
+    transform: rotateY(180deg);
+  }
+
+  .front, .back, .card-content {
+    width: 100%;
+    height: 100%;
+  }
+
+
+  .front, .back {
+    padding: 20px;
+    box-sizing: border-box;
+    backface-visibility: hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .front {
+    z-index: 2;
+    /* for firefox 31 */
+    transform: rotateY(0deg);
+  }
+  .back {
+    transform: rotateY(180deg);
+  }
 `
