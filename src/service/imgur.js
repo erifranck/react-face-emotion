060b15246aef499048f40a1ba4abc6399a928dfb
erifranck <erifranjose@hotmail.com>
2018-09-11 18:50:44 -0500

    get support for jpeg

--- src/service/imgur.js
+++ src/service/imgur.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const uploadImage = (img, clientId) => {
-  const formData = new FormData();
-  formData.append('image', img.split("data:image/png;base64,")[1])
+  const formData = new FormData()
+  formData.append('image', img.replace('data:image/png;base64,', '').replace('data:image/jpeg;base64,', ''))
   return axios
     .post('https://api.imgur.com/3/upload', formData, {
       headers: {
@@ -10,5 +10,4 @@ export const uploadImage = (img, clientId) => {
         'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
       }
     })
-
 }
