1c87f167d9a891def978da0a764c8b177f910931
erifranck <erifranjose@hotmail.com>
2018-09-10 21:42:58 -0500

    insert preview

--- src/components/ImageUploader/ImageUploader.js
+++ src/components/ImageUploader/ImageUploader.js
@@ -3,11 +3,18 @@ import UploaderComponent from './ImageUploader.jsx'
 
 export const ImageUploader = styled(UploaderComponent)`
   width: 400px;
-  height: 400px;
   img {
     max-width: 100%;
     max-height: 200px;
     display: block;
     margin: auto;
+  }
+  input[type=file] {
+    position: absolute;
+    top: 0px;
+    opacity: 0;
+    left: 0px;
+    height: 100%;
+    width: 100%;
   }
 `
--- src/components/ImageUploader/ImageUploader.jsx
+++ src/components/ImageUploader/ImageUploader.jsx
@@ -1,4 +1,6 @@
-import React, {Component} from 'react'
+import React, {
+  Component
+} from 'react'
 import PropTypes from 'prop-types'
 
 class ImageUploader extends Component {
@@ -8,10 +10,30 @@ class ImageUploader extends Component {
       url: ""
     }
     this.onChange = this.onChange.bind(this)
+    this.send = this.send.bind(this)
+    this.clear = this.clear.bind(this)
   }
 
-  onChange() {
+  clear() {
+    this.setState({
+      url: ""
+    })
+  }
 
+  send() {
+    this.props.upload(this.state.url)
+  }
+
+  onChange(event) {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      this.setState({
+        url: e.target.result
+      });
+    }
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+    }
   }
 
   render() {
@@ -24,14 +46,27 @@ class ImageUploader extends Component {
               <img src="upload.png" alt=""/>
           }
         </div>
-        <input type="file" onChange={this.onChange}/>
+        { !this.state.url && <input type="file" onChange={this.onChange}/> }
+        {
+          this.state.url && (
+            <div className="button-container">
+              <button onClick={this.clear} className="btn-secondary" >
+                Clear
+              </button>
+              <button onClick={this.send} className="btn-primary" >
+                Send
+              </button>
+            </div>
+          )
+        }
       </div>
     )
   }
 }
 
 ImageUploader.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  upload: PropTypes.func.isRequired
 }
 
 export default ImageUploader
