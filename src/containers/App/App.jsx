a5b943653233e311faf9eaeb1e52252de36e1a52
erifranck <erifranjose@hotmail.com>
2018-09-11 16:43:07 -0500

    insert new action

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -2,7 +2,7 @@ import React, {Fragment} from 'react'
 import PropTypes from 'prop-types'
 import {ImageUploader, Card, Form} from 'components'
 import {saveSettings} from 'redux/settings'
-import {saveImage, detectFace} from 'redux/emotion'
+import {saveImage, detectFace, detectEmotion} from 'redux/emotion'
 import {connect} from 'react-redux'
 
 const App = (props) => (
@@ -12,13 +12,18 @@ const App = (props) => (
       front={
         (
           <Fragment>
-            <h3 style={{textAlign: "center", margin: "10px 0px"}}>
+            <h3 style={{textAlign: 'center', margin: '10px 0px'}}>
               Upload your image to compare
             </h3>
-            <ImageUploader upload={props.saveImage}/>
+            <ImageUploader upload={props.saveImage} />
             {
               props.uploadImage &&
-                <button onClick={() => props.detectFace(props.uploadImage.link)}>Evaluate</button>
+                (
+                  <Fragment>
+                    <button onClick={props.detectFace(props.uploadImage.link)}>detect face</button>
+                    <button onClick={props.detectEmotion(props.uploadImage.link)}>detect emotion</button>
+                  </Fragment>
+                )
             }
           </Fragment>
         )
@@ -26,20 +31,18 @@ const App = (props) => (
       back={
         !props.settings ? (
           <Form
-            title="Insert your api information"
+            title='Insert your api information'
             onSubmit={props.saveSettings}
-            fields={
-              {
-                "subscription key": {
-                  value: "",
-                  type: "password"
-                },
-                "client id": {
-                  value: "",
-                  type: "password"
-                }
+            fields={{
+              'subscription key': {
+                value: '',
+                type: 'password'
+              },
+              'client id': {
+                value: '',
+                type: 'password'
               }
-            }
+            }}
           />
         ) : (
           <div>
@@ -54,6 +57,7 @@ const App = (props) => (
 App.propTypes = {
   className: PropTypes.string,
   detectFace: PropTypes.func,
+  detectEmotion: PropTypes.func,
   saveImage: PropTypes.func,
   saveSettings: PropTypes.func,
   settings: PropTypes.object,
@@ -64,4 +68,9 @@ export default connect(state => ({
   faceInfo: state.emotion.data,
   settings: state.settings.data,
   uploadImage: state.emotion.uploadImage
-}), {saveSettings, saveImage, detectFace})(App)
+}), dispatch => ({
+  saveSettings: (data) => dispatch(saveSettings(data)),
+  saveImage: (img) => dispatch(saveImage(img)),
+  detectFace: (img) => dispatch(detectFace(img)),
+  detectEmotion: (img) => dispatch(detectEmotion(img))
+}))(App)
