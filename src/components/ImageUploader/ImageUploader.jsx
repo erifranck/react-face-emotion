62125f2116e2217f04af0e513f3dcc924d5d04dc
erifranck <erifranjose@hotmail.com>
2018-09-20 09:40:38 -0500

    add .env file

--- src/components/ImageUploader/ImageUploader.jsx
+++ src/components/ImageUploader/ImageUploader.jsx
@@ -4,56 +4,56 @@ import React, {
 import PropTypes from 'prop-types'
 
 class ImageUploader extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
-      url: ""
+      url: ''
     }
     this.onChange = this.onChange.bind(this)
     this.send = this.send.bind(this)
     this.clear = this.clear.bind(this)
   }
 
-  clear() {
+  clear () {
     this.setState({
-      url: ""
+      url: ''
     })
   }
 
-  send() {
+  send () {
     this.props.upload(this.state.url)
   }
 
-  onChange(event) {
+  onChange (event) {
     const reader = new FileReader()
     reader.onload = (e) => {
       this.setState({
         url: e.target.result
-      });
+      })
     }
     if (event.target.files[0]) {
-      reader.readAsDataURL(event.target.files[0]);
+      reader.readAsDataURL(event.target.files[0])
     }
   }
 
-  render() {
+  render () {
     return (
       <div className={this.props.className}>
-        <div className="preview-container">
+        <div className='preview-container'>
           {
             this.state.url ?
-              <img src={this.state.url} alt="" /> :
-              <img src="upload.png" alt=""/>
+              <img src={this.state.url} alt='' /> :
+              <img src='upload.png' alt='' />
           }
         </div>
-        { !this.state.url && <input type="file" onChange={this.onChange}/> }
+        { !this.state.url && <input type='file' onChange={this.onChange} /> }
         {
           this.state.url && (
-            <div className="button-container">
-              <button onClick={this.clear} className="btn-secondary" >
+            <div className='button-container'>
+              <button onClick={this.clear} className='btn-secondary' >
                 Clear
               </button>
-              <button onClick={this.send} className="btn-primary" >
+              <button onClick={this.send} className='btn-primary' >
                 Send
               </button>
             </div>
