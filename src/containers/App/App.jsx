5ead85045665762aabb0b9dfe1dfc0f64572c0ff
erifranck <erifranjose@hotmail.com>
2018-09-20 08:40:13 -0500

    insert boolean prop

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react'
 import PropTypes from 'prop-types'
-import {ImageUploader, Card, Form, ImageFaceDetector} from 'components'
+import {ImageUploader, Card, Form} from 'components'
 import {saveSettings} from 'redux/settings'
 import {saveImage, detectFace} from 'redux/emotion'
 import {connect} from 'react-redux'
@@ -8,7 +8,7 @@ import {connect} from 'react-redux'
 const App = (props) => (
   <div className={props.className}>
     <Card
-      flip={!props.settings || props.faceInfo}
+      flip={(!props.settings || props.faceInfo) ? true : false}
       front={
         (
           <Fragment>
@@ -18,11 +18,7 @@ const App = (props) => (
             <ImageUploader upload={props.saveImage} />
             {
               props.uploadImage &&
-                (
-                  <Fragment>
-                    <button onClick={props.detectFace(props.uploadImage.link)}>detect face</button>
-                  </Fragment>
-                )
+                <button onClick={() => props.detectFace(props.uploadImage.link)}>Evaluate</button>
             }
           </Fragment>
         )
@@ -44,14 +40,9 @@ const App = (props) => (
             }}
           />
         ) : (
-          props.faceInfo &&
-            <ImageFaceDetector
-              link={props.uploadImage.link}
-              width={props.faceInfo[0].faceRectangle.width}
-              height={props.faceInfo[0].faceRectangle.height}
-              top={props.faceInfo[0].faceRectangle.top}
-              left={props.faceInfo[0].faceRectangle.left}
-            />
+          <div>
+            here is emoji
+          </div>
         )
       }
     />
@@ -61,11 +52,10 @@ const App = (props) => (
 App.propTypes = {
   className: PropTypes.string,
   detectFace: PropTypes.func,
-  detectEmotion: PropTypes.func,
   saveImage: PropTypes.func,
+  faceInfo: PropTypes.object,
   saveSettings: PropTypes.func,
   settings: PropTypes.object,
-  faceInfo: PropTypes.array,
   uploadImage: PropTypes.object
 }
 
@@ -73,8 +63,4 @@ export default connect(state => ({
   faceInfo: state.emotion.data,
   settings: state.settings.data,
   uploadImage: state.emotion.uploadImage
-}), dispatch => ({
-  saveSettings: (data) => dispatch(saveSettings(data)),
-  saveImage: (img) => dispatch(saveImage(img)),
-  detectFace: (img) => () => dispatch(detectFace(img))
-}))(App)
+}), {saveSettings, saveImage, detectFace})(App)
